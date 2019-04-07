f5abe4d05d02e75f5fea8c58363cb98c33758696
Przemyslaw Waberski <przemekwaberski@gmai.l.com>
2019-04-07 21:26:19 +0200

    Pobieranie danych z pliku

--- src/main/java/Saldo.java
+++ src/main/java/Saldo.java
@@ -1,21 +1,42 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Saldo {
 
-    private Scanner scanner;
-    int min, max, rate;
+    String line;
+    float min, max, rate;
+
+
+    boolean properAmount = false;
 
     public Saldo(String csvPath, float amount){
         try {
-            scanner = new Scanner(new File(csvPath));
-        } catch (FileNotFoundException e) {
+            BufferedReader reader = new BufferedReader(new FileReader(csvPath));
+
+            while ((line = reader.readLine()) != null){
+                String[] data = line.split(",");
+                if(amount > Float.parseFloat(data[0]) && amount < Float.parseFloat(data[1]){
+                    rate = Float.parseFloat(data[2]);
+                    properAmount = true;
+                }
+            }
+
+        } catch (java.io.IOException e) {
             System.out.println("Brak pliku zawierającego stopy oprocentowania lub plik uszkodzony");
         }
-
-
     }
+
+
+    public boolean isProperAmount() {
+        return properAmount;
+    }
+
+
 
 
 }
